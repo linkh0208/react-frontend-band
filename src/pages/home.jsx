3e6aea29a00f8eb281683b3c8e8f8792e2cb000f
link <linkh0208@gmail.com>
2024-03-30 00:54:48 -0400

    align text 3

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,8 @@ function Home() {
     return (
         <div className="home-page">
             <div className="titlecontainer">
-            <div className="title">Television Bonfire.</div>
+                <div className="title">Television</div>
+                <div className="title">Bonfire.</div>
             </div>
         </div>
     )
